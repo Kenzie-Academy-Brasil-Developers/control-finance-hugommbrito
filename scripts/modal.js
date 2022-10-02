9f056e58e9104dfc59d8166a4b7d6e7461285972
Hugo Brito <hugomdmb@gmail.com>
2022-10-02 00:32:37 -0300

    Segundo Commit - Adaptação mobile first para Desktop

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,12 @@
-/* Desenvolva sua lógica aqui */
+function openCloseModal(){
+    let modalButtons = document.querySelectorAll("[data-modal-control]")
+    let divModal = document.getElementById("modal")
+
+    modalButtons.forEach(button => {
+        button.addEventListener("click", function(){
+            divModal.classList.toggle("show-modal")
+        })
+    }) 
+
+}
+openCloseModal()
